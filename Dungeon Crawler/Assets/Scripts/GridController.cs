33a297a672504313f17cca4f3568efe94209de74
EmilWerminskiLMaS <werminski.e01@student.lmas.ac.uk>
2025-06-03 12:57:24 +0100

    added temp sprites

---
+++ Dungeon Crawler/Assets/Scripts/GridController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridController : MonoBehaviour {
+
+	private Grid grid;
+	[SerializeField] private Tilemap interaction;
+	[SerializeField] private Tile hoverTile;
+
+	private Vector3Int previousTilePosition = new Vector3Int ();
+
+	// Use this for initialization
+	void Start () {
+		grid = gameObject.GetComponent<Grid> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector3Int tilePosition = grid.WorldToCell (mousePosition);
+
+		if (Input.GetMouseButtonDown(0)) {
+			interaction.SetTile (previousTilePosition, null);
+			interaction.SetTile (tilePosition, hoverTile);
+			previousTilePosition = tilePosition;
+		}
+
+		Debug.DrawLine (GameObject.FindWithTag ("Player").transform.position, new Vector3 (previousTilePosition.x + 0.5f, previousTilePosition.y + 0.5f), Color.red);
+	}
+}
